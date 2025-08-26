153ee542caa29deb8424e418a1f6246b027da4be
xhesi <xhesi.galanxhi@outlook.com>
2025-08-26 11:13:33 +0200

    removed old references

--- dsl/procedures/generateJWTForAAPAndLaunchAndWaitJobTemplateWithSecureBody/steps/generateJWTForAAPAndLaunchAndWaitJobTemplateWithSecureBody.groovy
+++
@@ -1,4 +0,0 @@
-$[/myProject/groovy/scripts/preamble.groovy.ignore]
-
-ZeroTrust plugin = new ZeroTrust()
-plugin.runStep('generateJWTForAAPAndLaunchAndWaitJobTemplateWithSecureBody', 'generateJWTForAAPAndLaunchAndWaitJobTemplateWithSecureBody', 'generateJWTForAAPAndLaunchAndWaitJobTemplateWithSecureBody')
